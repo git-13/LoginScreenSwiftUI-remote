ac643ed40a3949a98da9d612960442b587f9c98e
Vladislav <git13vl@gmail.com>
2022-06-30 16:07:08 +0200

    Initial Commit

---
+++ LoginScreenSwiftUI/LoginScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenSwiftUIApp.swift
+//  LoginScreenSwiftUI
+//
+//  Created by Vladislav Glovatskiy on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
